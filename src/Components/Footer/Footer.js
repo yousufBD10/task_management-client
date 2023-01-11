e5389b6c724cde384a5c369b5f99c6bb2cbbbf24
dev-mahfuz <mrsohag04@gmail.com>
2023-01-12 00:38:05 +0600

    Footer added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,9 +2,28 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className="footer p-10 bg-base-200 text-base-content">
+            <div>
+                <img src='/logo.png' className="fill-current" />
+                <p>TaskMaster<br />Task Management System</p>
+            </div>
+            <div>
+                <span className="footer-title">Services</span>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Marketing</a>
+            </div>
+            <div>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </div>
+        </footer>
     );
 };
 
