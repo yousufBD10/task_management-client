c880a3999a6e4f38d07902af53a4d9007291461c
Forhad <farhadhossain01992@gmail.com>
2023-01-13 02:10:24 +0600

    Banner is ok

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,8 +6,7 @@ import taskMaster from '../assets/banner/taskMaster.png';
 const Header = () => {
     return (
         <div className='hello bg-gradient-to-r from-blue-700 to-indigo-300 lg:h-[84vh] grid lg:grid-cols-2 px-12'>
-            <div>
-              
+            <div>        
                 <h1 className='text-6xl font-serif font-bold mt-12 pl-6'>One app to <br /> replace them all.</h1>
                 <h3 className='text-xl font-semibold font-serif pl-6 mt-8'>We’re more than a doc. Or a table. Customize Notion <br /> to work the way you do.</h3>
               <form className='pl-6'>
