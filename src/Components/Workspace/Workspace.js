746d1fead2f8e90c162e968ead8c77e7194dc0a1
yousufBD10 <yousufhassan.yh2@gmail.com>
2023-01-16 15:52:49 +0600

    side bar added

--- src/Components/Workspace/Workspace.js
+++ src/Components/Workspace/Workspace.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { BiUser } from "react-icons/bi";
+import { AiOutlineSetting } from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
 const Workspace = () => {
     return (
-        <div>
-            
+        <div className=' mt-3'>
+            <div className="drawer drawer-mobile">
+  <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+  <div className="drawer-content flex flex-col items-center justify-center">
+    
+    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+  
+  </div> 
+  <div className="drawer-side  shadow-md">
+    <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
+    <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+    
+     <Link to='/workspace/boards' > <li className='shadow-sm mb-2 rounded-full'><a> <MdOutlineSpaceDashboard/>Boards</a></li></Link>
+      <Link to='/workspace/members'><li className='shadow-sm mb-2 rounded-full'><a> <BiUser/>Members</a></li></Link>
+     <Link to='/workspace/settings'> <li className='shadow-sm mb-2 rounded-full'><a> <AiOutlineSetting/>Settings</a></li></Link>
+     
+     
+    </ul>
+  
+  </div>
+</div>
         </div>
     );
 };
