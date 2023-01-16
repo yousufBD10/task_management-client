d69843b3e22a49191720ff0c8be1576199222b33
dev-mahfuz <mrsohag04@gmail.com>
2023-01-16 13:34:03 +0600

    folder structure, navbar modify and route setup

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -2,6 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Homepage from "../Components/Homepage/Homepage";
 import Login from "../Components/Login/Login";
 import SingUp from "../Components/SignUp/SignUp";
+import UserProfile from "../Components/User/UserProfile";
+import Boards from "../Components/Workspace/Boards";
+import CreateWordspace from "../Components/Workspace/CreateWordspace";
+import Members from "../Components/Workspace/Members";
+import Settings from "../Components/Workspace/Settings";
+import App from "../Layout/App";
 import Main from "../Layout/Main";
 
 const Router = createBrowserRouter([
@@ -20,6 +26,32 @@ const Router = createBrowserRouter([
       {
         path: "/login",
         element: <Login></Login>,
+      },
+      {
+        path: "/profile",
+        element: <UserProfile />,
+      },
+    ],
+  },
+  {
+    path: "/workspace",
+    element: <App />,
+    children: [
+      {
+        path: "/workspace/boards",
+        element: <Boards />,
+      },
+      {
+        path: "/workspace/settings",
+        element: <Settings />,
+      },
+      {
+        path: "/workspace/members",
+        element: <Members />,
+      },
+      {
+        path: "/workspace/create",
+        element: <CreateWordspace />,
       },
     ],
   },
