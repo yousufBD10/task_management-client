af2227a76c7fcf6516a1d065db407af1bb174e35
dev-mahfuz <mrsohag04@gmail.com>
2023-01-17 13:08:31 +0600

    login and role hook

---
+++ src/hooks/useRole.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+
+const useRole = email => {
+    const [role, setRole] = useState(false);
+    const [isRoleLoading, setRoleLoading] = useState(true);
+    useEffect(() => {
+        if (email) {
+            fetch(`${process.env.REACT_APP_SERVER_URL}/getRole`, {
+                method: 'POST',
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => res.json())
+                .then(data => {
+                    localStorage.setItem('role', data.role);
+                    setRole(data.role);
+                    setRoleLoading(false);
+                })
+        }
+    }, [email])
+    return [role, isRoleLoading]
+}
+
+export default useRole;
