369cb7d203808bee85849339dc61a7111a3a6911
hasibferdous <hasibferdous.ict@gmail.com>
2023-01-18 13:34:46 +0600

    member section added

--- src/Components/Workspace/Members.js
+++ src/Components/Workspace/Members.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import WorkspaceNameAndInvite from './WorkspaceNameAndInvite';
 
 const Members = () => {
+    const members = [
+        {
+          image: '',
+          name: 'Hasib Ferdous',
+          id:'hasibferdous09'
+        },
+        {
+          image: '',
+          name: 'Farhad Hossain',
+          id:'farhadhossain19'
+
+        },
+
+      ]
     return (
-        <div>
+        <div className=' min-h-screen '>
+            <WorkspaceNameAndInvite></WorkspaceNameAndInvite>
+            <h1 className="text-xl font-medium px-5">Members</h1>
+        <div className='flex flex-row'>
+            <div className='flex flex-col w-60 g-3'>
+            <button className='btn btn-primary btn-sm rounded-sm mt-3 mb-3'>Workspace members</button>
+            <button className='btn btn-primary btn-sm rounded-sm mb-3'>Guests</button>
+            <button className='btn btn-primary btn-sm rounded-sm mb-3'>Pending</button>
+            </div>
+            <div>
+                <h3 className="text-xl font-medium px-5 mt-2">Workspace members</h3>  
+                <h6 className='ml-5'>Workspace members can view and join all workspace visible <br/> boards and create new boards in the workspace. </h6>  
+            </div>
+        </div>
+        <div className='flex flex-row'>
+        <div className="form-control w-60 max-w-xs mt-3">
+            <label className="label">
+              <span className="label-text font-medium">Filter by</span>
+            </label>
+            <select className="select select-bordered rounded-sm select-sm">
+              <option>Name</option>
+              <option>Date</option>
+              <option>Contribution</option>
+              <option>Roles</option>             
+            </select>
+          </div>
+            <div>
+            {
+          members.map((member, i) =>
+            <div className="flex flex-row card ml-5 mt-6 bg-base-50">
+              <img className='w-12 h-12 rounded-xl' src={member.image} alt="" />
+              <div className='mr-5 w-56'>
+              <h2 className="card-title ml-2">{member.name}</h2>
+              <h2 className="card-id ml-2">{member.id}</h2>
+              </div>
+              <div className='ml-11'>
+              <button className='btn-success px-2 m-2 rounded-md'>Make Admin</button>
+              <button className='btn-info px-2 m-2 rounded-md'>Set Role</button>
+              <button className='btn-primary px-3 m-2 rounded-md'>Remove</button>
+
+              </div>
+
+            </div>
+          )
+        }
             
+
+            </div>
+        </div>
+
         </div>
     );
 };
