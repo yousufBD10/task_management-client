72261e5ffa0640c7041ef75c91f83dccdd93371f
Anik Kumar Nath (Web) <108430535+aknathweb@users.noreply.github.com>
2023-02-13 21:38:46 +0600

    update checklist

--- src/Components/CheckList/CheckList.js
+++ src/Components/CheckList/CheckList.js
@@ -1,60 +1,89 @@
 import React, { useEffect, useState } from 'react';
 import CheckListItem from './CheckListItem';
 
-const taskCheck = [
+/**
+ * get data from database base on below parameters
+ *  wid: 1,
+ * boradId: 1,
+ * taskId: 1,
+ */
+let taskCheck = [
     {
-        workBoardId: 1,
-        boardCardID: 1,
-        taskDataID: 1,
+        wid: 1,
+        boradId: 1,
+        taskId: 1,
+        _id: 1,
         taskDoneActivity: true,
         taskText: 'Initial Task',
         taskCheckAssignDate: new Date(),
     },
     {
-        workBoardId: 1,
-        boardCardID: 1,
-        taskDataID: 1,
+        wid: 1,
+        boradId: 1,
+        taskId: 1,
+        _id: 2,
         taskDoneActivity: true,
         taskText: 'Second Task',
         taskCheckAssignDate: new Date(),
     },
     {
-        workBoardId: 1,
-        boardCardID: 1,
-        taskDataID: 1,
+        wid: 1,
+        boradId: 1,
+        taskId: 1,
+        _id: 3,
         taskDoneActivity: true,
         taskText: 'Third Task',
         taskCheckAssignDate: new Date(),
     },
     {
-        workBoardId: 1,
-        boardCardID: 1,
-        taskDataID: 1,
+        wid: 1,
+        boradId: 1,
+        taskId: 1,
+        _id: 4,
         taskDoneActivity: false,
         taskText: 'Fourth Task',
         taskCheckAssignDate: new Date(),
     },
     {
-        workBoardId: 1,
-        boardCardID: 1,
-        taskDataID: 1,
+        wid: 1,
+        boradId: 1,
+        taskId: 1,
+        _id: 5,
         taskDoneActivity: true,
         taskText: 'Last Task',
         taskCheckAssignDate: new Date(),
     },
 ]
-const CheckList = () => {
+const CheckList = ({ currentTask }) => {
     const [totalSelectedCheckboxes, setTotalSelectedCheckboxes] = useState(0);
+    const [change, setChange] = useState(false)
+    const handleNewTaskCheckAdd = (e) => {
+        // post task on database
+        e.preventDefault();
+        const assignCheckListTask = e.target.assignTask.value;
+        // console.log data send on database
+        console.log({
+            wid: currentTask.wid,
+            boradId: currentTask.boradId,
+            taskId: currentTask._id,
+            CheckListTask: assignCheckListTask,
+            taskDoneActivity: false,
+            taskCheckAssignDate: new Date()
+        })
+        e.target.reset();
+
+    }
+    const handleTaskCheckChange = (taskId) => {
+        // update task on database _id===taskId and set taskDoneActivity: !task.taskDoneActivity
+        taskCheck = [...taskCheck.map(task => task._id !== taskId ? task : { ...task, taskDoneActivity: !task.taskDoneActivity })]
+    }
     /**
      * handle check mainly check number of checkbox items are checked 
-     * under checkTask class
+     * and recheck after any change
      *  */
-    function handleCheck() {
-        setTotalSelectedCheckboxes(document.querySelectorAll('.checkTask input[type=checkbox]:checked').length);
-    }
     useEffect(() => {
-        handleCheck();
-    }, [])
+        setTotalSelectedCheckboxes(taskCheck.filter(task => task.taskDoneActivity).length);
+    }, [change])
     return (
         <div>
             {/* Calculate percentage of checked items and show a progress bar */}
@@ -74,23 +103,24 @@ const CheckList = () => {
                     {/* show all assign checklist tasks */}
                     <div className='checkTask'>
                         {
-                            taskCheck.map((item, i) => <CheckListItem key={i} text={item.taskText} taskDoneActivity={item.taskDoneActivity} handleCheck={handleCheck} />)
+                            taskCheck.map((item, i) => <CheckListItem key={i} text={item.taskText} taskDoneActivity={item.taskDoneActivity} taskId={item._id} handleCheck={handleTaskCheckChange} setChange={setChange} change={change} />)
                         }
                     </div>
                     {/* Static checklist tasks end*/}
                     {/* new checklist task Assign area  */}
-                    <form className="flex flex-col py-6 space-y-4 md:py-0 ng-untouched ng-pristine ng-valid">
+                    <form onSubmit={handleNewTaskCheckAdd} className="flex flex-col py-6 space-y-4 md:py-0 ng-untouched ng-pristine ng-valid">
                         <label className="block">
                             <h3 className="text-md font-semibold">New Task Assign</h3>
                             <textarea
+                                required
                                 placeholder="Write a Task..."
+                                name='assignTask'
                                 className="input input-bordered p-2 mt-2 w-full rounded-sm outline-border"
                             ></textarea>
                         </label>
                         <div className="flex flex-wrap">
                             {/* ----- Task Submit button------ */}
                             <button
-                                onClick={""}
                                 type="submit"
                                 className="btn btn-ghost btn-sm rounded-md bg-gray-800 text-gray-400"
                             >
