82ef2775dbe4da97fd2354586698bb60cd205e92
Anik Kumar Nath (Web) <108430535+aknathweb@users.noreply.github.com>
2023-02-23 14:19:12 +0600

    dynamic checklist

--- src/Components/CheckList/CheckListItem.js
+++ src/Components/CheckList/CheckListItem.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const CheckListItem = ({ text, handleCheck, taskId, taskDoneActivity, setChange, change }) => {
-    const [check, setCheck] = useState(taskDoneActivity)
+const CheckListItem = ({ text, handleCheckListUpdate, checkListItemId, taskDoneActivity, taskId, handleRemoveChecklist }) => {
     return (
-        <div className='flex items-start justify-start gap-2 my-2'>
-            <input type="checkbox" defaultChecked={taskDoneActivity || false} onChange={(e) => { setCheck(e.target.checked); handleCheck(taskId); setChange(!change) }} />
-            <textarea className={`${check && 'line-through'} w-full px-1`}>{text}</textarea>
+        <div className='flex justify-between'>
+            <div className='flex items-center justify-start gap-2 my-2'>
+                <input type="checkbox" checked={taskDoneActivity} onChange={() => { handleCheckListUpdate(checkListItemId, taskId, taskDoneActivity) }} />
+                <span className={`${taskDoneActivity && 'line-through'} w-full px-1`}>{text}</span>
+            </div>
+            <div>
+                <button className='btn btn-ghost p-0 ' onClick={() => handleRemoveChecklist(checkListItemId, taskId)}>✂</button>
+            </div>
         </div>
     );
 };
